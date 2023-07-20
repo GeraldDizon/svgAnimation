22f8c2819a2225cdaaa24116b9493691507b5b98
GeraldDizon <geralddizon84@yahoo.com>
2023-07-20 16:41:37 +0800

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     var animation = bodymovin.loadAnimation({
         container: document.getElementById('imageSVG'),
         renderer: 'svg',
-        loop: false,
+        loop: true,
         autoplay: true,
         path: 'images/BodyMovin.json'
     })
