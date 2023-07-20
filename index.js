90e8ac905df817b6a1746c034c5f4699e98a6b3e
GeraldDizon <geralddizon84@yahoo.com>
2023-07-20 16:16:31 +0800

    changes

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 $( document ).ready(function() {
-    $(function () {
-        $(document).scroll(function () {
-            var $nav = $(".fixed-top");
-            var $logo = $(".navbar-brand");
-            $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-            $logo.toggleClass('colored', $(this).scrollTop() > $nav.height());
-        });
-    });
     var animation = bodymovin.loadAnimation({
         container: document.getElementById('imageSVG'),
         renderer: 'svg',
         loop: false,
         autoplay: true,
-        path: 'images/data.json'
-      })
+        path: 'images/ggwp.json'
+    })
 });
 
